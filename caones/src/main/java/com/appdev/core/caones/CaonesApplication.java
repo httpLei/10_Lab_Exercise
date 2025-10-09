97de9dc4294b8ddf83cc8e7a30dce72165a822f7
lei <lerahcaones18@gmail.com>
2025-10-09 19:11:03 +0800

    initial commit

---
+++ caones/src/main/java/com/appdev/core/caones/CaonesApplication.java
@@ -0,0 +1,13 @@
+package com.appdev.core.caones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaonesApplication.class, args);
+	}
+
+}
---
+++ caones/src/test/java/com/appdev/core/caones/CaonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.core.caones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
